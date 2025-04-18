6ba5c6cecf9dbfdf91b75f600772da96ea90b321
Vlad Chevdar <143352246+VladChevdar@users.noreply.github.com>
2025-04-18 00:58:02 -0700

    Add files via upload

---
+++ publisher.py
@@ -0,0 +1,24 @@
+from google.cloud import pubsub_v1
+import json, time
+
+project_id = "mov-data-eng"
+topic_id = "my-topic"
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_id)
+
+with open("bcsample.json") as f:
+    records = json.load(f)
+
+start = time.time()
+futures = [
+    publisher.publish(topic_path, json.dumps(r).encode("utf-8"))
+    for r in records
+]
+
+# Wait for all publish operations to complete
+for f in futures:
+    f.result()
+
+elapsed = time.time() - start
+print(f"✅ Published {len(records)} messages in {elapsed:.2f}s")
+
---
+++ subscriber.py
@@ -0,0 +1,39 @@
+from concurrent.futures import TimeoutError
+from google.cloud import pubsub_v1
+import time
+import threading
+
+project_id = "mov-data-eng"
+subscription_id = "my-sub"
+expected_message_count = 5385
+
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+counter = 0
+start = time.time()
+done = threading.Event()
+
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    global counter
+    counter += 1
+    message.ack()
+    
+    if counter >= expected_message_count:
+        done.set()
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"🔔 Listening for messages on {subscription_path}...\n")
+
+done.wait(timeout=30)
+
+end = time.time()
+elapsed = end - start
+
+# Clean up the subscriber
+streaming_pull_future.cancel()
+subscriber.close()
+
+print(f"🟧 Total messages received: {counter}")
+print(f"🟧 Time elapsed: {elapsed:.2f} seconds.")
+
